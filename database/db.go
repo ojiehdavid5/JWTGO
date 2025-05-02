836f2368aab4b2a56a3a3e2c5869dda686e1f146
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-05-02 23:07:19 +0100

    model

---
+++ database/db.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+ "fmt"
+ "github.com/chuks/JWTGO/model"
+ "os"
+
+ "gorm.io/gorm"
+ "gorm.io/driver/postgres"
+
+)
+
+// DB instance
+var DB *gorm.DB
+
+// Connect to the database
+func Connect() {
+ dsn := fmt.Sprintf(
+  "host=localhost port=%s user=%s password=%s dbname=%s sslmode=disable",
+  os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"),
+ )
+
+ DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+ if err != nil {
+  panic("failed to connect database")
+ }
+ fmt.Println("Connection Opened to Database")
+
+ // Migrate the schemas
+ DB.AutoMigrate(&model.Book{}, &model.User{})
+ fmt.Println("Database Migrated")
+}
---
+++ model/user.go
@@ -0,0 +1,6 @@
+package model
+type User struct {
+	ID           uint   `gorm:"primaryKey" json:"id"`
+	Email        string `gorm:"not null" json:"email"`
+	PasswordHash string `gorm:"not null" json:"password_hash,omitempty"`
+   }
