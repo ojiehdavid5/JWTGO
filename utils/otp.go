6c21ece3f1f60451935c77ce55bffd25065f98b5
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-05-13 13:05:40 +0100

    still working on tests

--- utils/otp.go
+++ utils/otp.go
@@ -14,6 +14,8 @@ func GenerateOTP(length int) (string, error) {
 	const digits = "0123456789"
 	otp := make([]byte, length)
 	for i := range otp {
+
+		
 		randInt, err := rand.Int(rand.Reader, big.NewInt(int64(len(digits))))
 		if err != nil {
 			return "", err
